660a1755d23c4d322d622e284084bd26d3286018
kimjiahn <jiahnkimm@gmail.com>
2020-03-18 21:59:40 +0900

    nicos gameEx 2-1

--- js/paintJs/paintJs.js
+++ js/paintJs/paintJs.js
@@ -1,27 +1,39 @@
 const canvas = document.querySelector("#js-Canvas");
+//canvas 안에서 pixel에 접근하기위한 변수선언
+const ctx = canvas.getContext("2d");
+//canvas 안에서 픽셀을 다루기 위한
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
 function stopPainting() {
     painting = false;
 }
-//mouse in canvas 이벤트
+function startPainting() {
+    painting = true;
+}
+//mouse in canvas 이벤트(움직임 감지)
 function onMouseMove(event) {
-    // console.log(event);
+    console.log(event);
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 //mouse down canvas 이벤트 (마우스를 누르고 있는 상태)
 function onMouseDown(event) {
     painting = true;
     // console.log(event);
 }
-function onmouseup(event) {
-    stopPainting();
-}
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onmouseup);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
