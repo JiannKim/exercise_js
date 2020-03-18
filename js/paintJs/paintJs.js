997a388ba3fdfab489abd4bd3d11ea818127040f
kimjiahn <jiahnkimm@gmail.com>
2020-03-18 21:15:22 +0900

    nicos gsmeEx 2

--- js/paintJs/paintJs.js
+++ js/paintJs/paintJs.js
@@ -1,1 +1,27 @@
-fdsfsdfsd
+const canvas = document.querySelector("#js-Canvas");
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+//mouse in canvas 이벤트
+function onMouseMove(event) {
+    // console.log(event);
+    const x = event.offsetX;
+    const y = event.offsetY;
+}
+//mouse down canvas 이벤트 (마우스를 누르고 있는 상태)
+function onMouseDown(event) {
+    painting = true;
+    // console.log(event);
+}
+function onmouseup(event) {
+    stopPainting();
+}
+if(canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onmouseup);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
