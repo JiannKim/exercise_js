db50e6a2dd12f1e865442cbffcb416bca648bb5a
kimjiahn <jiahnkimm@gmail.com>
2020-03-19 17:59:58 +0900

    nicos GameEx 2-5

--- js/paintJs/paintJs.js
+++ js/paintJs/paintJs.js
@@ -2,14 +2,23 @@ const canvas = document.getElementById("js-Canvas");
 const colors = document.getElementsByClassName("js-Color");
 const range = document.getElementById("js-Range");
 const mode = document.getElementById("js-Mode");
+
 //canvas 안에서 pixel에 접근하기위한 변수선언
 const ctx = canvas.getContext("2d");
+//default value
+const Inition_Color = "#2c2c2c";
+const Canvas_Width = 700;  //적용될 픽셀(canvas) 사이즈
+const Canvas_Height = 650;
+
 //적용될 픽셀(canvas) 사이즈
-canvas.width = 700;
-canvas.height = 650;
+canvas.width = Canvas_Width;
+canvas.height = Canvas_Height;
 //canvas 안에서 픽셀을 다루기 위한
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = Inition_Color;
+ctx.fillStyle = Inition_Color;
 ctx.lineWidth = 5;
+//fill mode
+ctx.fillStyle = "color";
 
 let painting = false;
 let filling = false;
@@ -30,12 +39,13 @@ function onMouseMove(event) {
     } else {    //클릭할경우(painting이 true일경우) stroke한다
         ctx.lineTo(x, y);
         ctx.stroke();
-    }
+    };
 }
 //brush의 색상을 변경해주기 위한 메소드
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 //range조절시 brush굵기 반영위한 메소드
 function handleRangeChange(event) {
@@ -48,17 +58,24 @@ function handleModeClick(event) {
     if(filling === true) {
         filling = false;
         mode.innerText = "Fill";
+        ctx.fillStyle
     } else {
         filling = true;
         mode.innerText = "PAINT"
-    }
+    };
+}
+function handleCanvasClick(event) {
+    if(filling) {
+        ctx.fillRect(0, 0, Canvas_Width, Canvas_Height);
+    } else {};
 }
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
-}
+    canvas.addEventListener("click", handleCanvasClick);
+};
 //handleColorClick에 인수값을 할당해주기
 Array.from(colors).forEach(color =>
     color.addEventListener("click", handleColorClick)
@@ -70,4 +87,4 @@ if(range) {
 //button(mode)에 연결
 if(mode) {
     mode.addEventListener("click", handleModeClick);
-}
+};
