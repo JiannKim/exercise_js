4c5ceac2a7f0d1d9160f1f718e406795d291090c
kimjiahn <jiahnkimm@gmail.com>
2020-03-30 17:46:42 +0900

    node study

--- webNode/main.js
+++ webNode/main.js
@@ -58,7 +58,7 @@ var app = http.createServer(function (request, response) {
                 <h2>${title}</h2>${description}`,
                 `<a href="/create">CREATE</a>
                 <a href="/update?id=${title}">UPDATE</a>
-                <form>
+                <form action="/delete_process" method="post" onsubmit="lalala">
                     <input type="hidden" name="id" value="${title}" />
                     <input type="submit" value="DELETE" />
                 </form>
@@ -132,6 +132,20 @@ var app = http.createServer(function (request, response) {
                 })
                 });
         })
+    } else if(pathname === '/delete_process') {
+        var body = '';
+        request.on('data', function(data) {
+            body = body + data;
+        });
+        request.on('end', function() {
+            var post = qs.parse(body);
+            var id = post.id;
+
+            fs.unlink(`data/${id}`, function(err) {
+                response.writeHead(302, {Location: `/`});
+                response.end('');
+            });
+         });
     } else {
         response.writeHead(404);
         response.end('Not found!!!');
