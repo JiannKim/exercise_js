068bebc729e1ee189ed57731dfed4c386a91897a
kimjiahn <jiahnkimm@gmail.com>
2020-04-02 17:11:53 +0900

    ts study no.3

--- TS/index.js
+++ TS/index.js
@@ -1,2 +1,9 @@
-console.log('hello world!');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.name = "dominica", exports.age = 29, exports.gender = "woman";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello! ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(exports.name, exports.age, exports.gender);
+// export {};
 //# sourceMappingURL=index.js.map
--- TS/index.ts
+++ TS/index.ts
@@ -1,1 +1,9 @@
-console.log('hello world!');
+export const name = "dominica",
+    age = 29,
+    gender = "woman";
+
+const sayHi = (name, age, gender) => {
+    console.log(`Hello! ${name}, you are ${age}, you are a ${gender}`);
+}
+sayHi(name, age, gender);
+// export {};
