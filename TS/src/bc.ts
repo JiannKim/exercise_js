c7fa95edd5ba151b0f002a8b7599df6d49cf7a0a
kimjiahn <jiahnkimm@gmail.com>
2020-04-03 23:10:53 +0900

    ts study 9

--- TS/src/bc.ts
+++ TS/src/bc.ts
@@ -42,4 +42,11 @@ const getLatestBlock = () : Block => blockchain[blockchain.length -1];
 
 const getNewTimeStamp = () : number => Math.round(new Date().getTime()/1000);
 
+const createNewBlock = (data:string): Block => {
+    const previousBlock : Block = getLatestBlock();
+    const newIndex : number = previousBlock + 1;
+    const newTimeStamp : number = getNewTimeStamp(); 
+    const newHash : string = Block.calculateBlockHash();
+}
+
 export {};
